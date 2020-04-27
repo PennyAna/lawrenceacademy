765cea81e4e08ded8f2ef98d4d80ab51c6b5396b
Nicole <lawrence.restrainedchaos@gmail.com>
2020-04-26 19:06:55 -0600

    team2

---
+++ web/team2.js
@@ -0,0 +1,10 @@
+
+function changeColor() {
+	var colorChoice = document.getElementById("choice");
+	var divIt = document.getElementById("div1");
+	divIt.style.color = colorChoice;
+}
+
+function clickMe() {
+	alert("Clicked!");
+}
